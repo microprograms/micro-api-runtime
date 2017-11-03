a53628b93da3946f9849673023e31ded78a56176
xuzewei <xuzewei_2012@126.com>
2017-11-03 15:31:25 +0800

    v1.0.2

--- src/main/java/com/github/microprograms/micro_api_runtime/annotation/MicroApiAnnotation.java
+++ src/main/java/com/github/microprograms/micro_api_runtime/annotation/MicroApiAnnotation.java
@@ -6,15 +6,13 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import com.github.microprograms.micro_api_runtime.enums.MicroApiTypeEnum;
-
 @Retention(RUNTIME)
 @Target(TYPE)
 public @interface MicroApiAnnotation {
     /**
      * @return 类型
      */
-    MicroApiTypeEnum type();
+    String type();
 
     /**
      * @return 版本
--- src/main/java/com/github/microprograms/micro_api_runtime/enums/MicroApiTypeEnum.java
+++ src/main/java/com/github/microprograms/micro_api_runtime/enums/MicroApiTypeEnum.java
@@ -8,5 +8,14 @@ public enum MicroApiTypeEnum {
     /**
      * 读写
      */
-    write
+    write;
+
+    public static MicroApiTypeEnum parse(String string) {
+        for (MicroApiTypeEnum x : values()) {
+            if (x.toString().equalsIgnoreCase(string)) {
+                return x;
+            }
+        }
+        return null;
+    }
 }
