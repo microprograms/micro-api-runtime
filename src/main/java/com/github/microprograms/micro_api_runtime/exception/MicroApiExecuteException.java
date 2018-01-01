0646a48b86dd1ecceba43d490b4d96683e4251b0
xuzewei <xuzewei_2012@126.com>
2018-01-01 14:41:22 +0800

    v1.0.8

--- src/main/java/com/github/microprograms/micro_api_runtime/exception/MicroApiExecuteException.java
+++ src/main/java/com/github/microprograms/micro_api_runtime/exception/MicroApiExecuteException.java
@@ -1,5 +1,7 @@
 package com.github.microprograms.micro_api_runtime.exception;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.github.microprograms.micro_api_runtime.model.ResponseCode;
 
 public class MicroApiExecuteException extends Exception {
@@ -7,13 +9,21 @@ public class MicroApiExecuteException extends Exception {
     private static final String format = "(%d) %s";
     private final ResponseCode responseCode;
 
-    public MicroApiExecuteException(ResponseCode responseCode, Throwable cause) {
-        super(String.format(format, responseCode.getCode(), responseCode.getMessage()), cause);
-        this.responseCode = responseCode;
+    public MicroApiExecuteException(ResponseCode responseCode) {
+        this(responseCode, null, null);
     }
 
-    public MicroApiExecuteException(ResponseCode responseCode) {
-        this(responseCode, null);
+    public MicroApiExecuteException(ResponseCode responseCode, String rewriteMessage) {
+        this(responseCode, rewriteMessage, null);
+    }
+
+    public MicroApiExecuteException(ResponseCode responseCode, Throwable cause) {
+        this(responseCode, null, cause);
+    }
+
+    public MicroApiExecuteException(ResponseCode responseCode, String rewriteMessage, Throwable cause) {
+        super(String.format(format, responseCode.getCode(), StringUtils.isNotBlank(rewriteMessage) ? rewriteMessage : responseCode.getMessage()), cause);
+        this.responseCode = responseCode;
     }
 
     public ResponseCode getResponseCode() {
