d4fbb1ffae21b7e2019a1aca65ab1900db0c7ea5
xuzewei <microprograms@126.com>
2019-07-15 14:18:53 +0800

    v5.0.0

--- src/main/java/com/github/microprograms/micro_api_runtime/model/Request.java
+++ src/main/java/com/github/microprograms/micro_api_runtime/model/Request.java
@@ -1,43 +1,52 @@
 package com.github.microprograms.micro_api_runtime.model;
 
-import com.github.microprograms.micro_nested_data_model_runtime.Comment;
-import com.github.microprograms.micro_nested_data_model_runtime.Required;
-
 public class Request {
-    @Comment("API接口名字") @Required(true) private String apiName;
-    @Comment("请求唯一标识符") @Required(false) private String uuid;
-    @Comment("请求发送时间") @Required(false) private long sendToEngineTimestamp;
-    @Comment("请求被服务器开始处理的时间戳") @Required(false) private long executeByEngineTimestamp;
+	/**
+	 * API接口名字
+	 */
+	private String apiName;
+	/**
+	 * 请求唯一标识符
+	 */
+	private String uuid;
+	/**
+	 * 请求发送时间
+	 */
+	private long sendToEngineTimestamp;
+	/**
+	 * 请求被服务器开始处理的时间戳
+	 */
+	private long executeByEngineTimestamp;
 
-    public String getApiName() {
-        return apiName;
-    }
+	public String getApiName() {
+		return apiName;
+	}
 
-    public void setApiName(String apiName) {
-        this.apiName = apiName;
-    }
+	public void setApiName(String apiName) {
+		this.apiName = apiName;
+	}
 
-    public String getUuid() {
-        return uuid;
-    }
+	public String getUuid() {
+		return uuid;
+	}
 
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
 
-    public long getSendToEngineTimestamp() {
-        return sendToEngineTimestamp;
-    }
+	public long getSendToEngineTimestamp() {
+		return sendToEngineTimestamp;
+	}
 
-    public void setSendToEngineTimestamp(long sendToEngineTimestamp) {
-        this.sendToEngineTimestamp = sendToEngineTimestamp;
-    }
+	public void setSendToEngineTimestamp(long sendToEngineTimestamp) {
+		this.sendToEngineTimestamp = sendToEngineTimestamp;
+	}
 
-    public long getExecuteByEngineTimestamp() {
-        return executeByEngineTimestamp;
-    }
+	public long getExecuteByEngineTimestamp() {
+		return executeByEngineTimestamp;
+	}
 
-    public void setExecuteByEngineTimestamp(long executeByEngineTimestamp) {
-        this.executeByEngineTimestamp = executeByEngineTimestamp;
-    }
+	public void setExecuteByEngineTimestamp(long executeByEngineTimestamp) {
+		this.executeByEngineTimestamp = executeByEngineTimestamp;
+	}
 }
--- src/main/java/com/github/microprograms/micro_api_runtime/model/Response.java
+++ src/main/java/com/github/microprograms/micro_api_runtime/model/Response.java
@@ -3,84 +3,101 @@ package com.github.microprograms.micro_api_runtime.model;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import com.github.microprograms.micro_api_runtime.enums.MicroApiReserveResponseCodeEnum;
-import com.github.microprograms.micro_nested_data_model_runtime.Comment;
-import com.github.microprograms.micro_nested_data_model_runtime.Required;
 
 public class Response {
-    @Comment("API接口名字") @Required(true) private String apiName;
-    @Comment("请求唯一标识符") @Required(true) private String requestUuid;
-    @Comment("错误码") @Required(true) private int code = MicroApiReserveResponseCodeEnum.success.getCode();
-    @Comment("错误提示") @Required(false) private String message = MicroApiReserveResponseCodeEnum.success.getMessage();
-    @Comment("错误堆栈") @Required(false) private String stackTrace;
-    @Comment("请求被服务器处理完毕的时间戳") @Required(true) private long overByEngineTimestamp;
+	/**
+	 * API接口名字
+	 */
+	private String apiName;
+	/**
+	 * 请求唯一标识符
+	 */
+	private String requestUuid;
+	/**
+	 * 错误码
+	 */
+	private int code = MicroApiReserveResponseCodeEnum.success.getCode();
+	/**
+	 * 错误提示
+	 */
+	private String message = MicroApiReserveResponseCodeEnum.success.getMessage();
+	/**
+	 * 错误堆栈
+	 */
+	private String stackTrace;
+	/**
+	 * 请求被服务器处理完毕的时间戳
+	 */
+	private long overByEngineTimestamp;
 
-    public void success() {
-        this.code = MicroApiReserveResponseCodeEnum.success.getCode();
-        this.message = MicroApiReserveResponseCodeEnum.success.getMessage();
-    }
+	public void success() {
+		this.code = MicroApiReserveResponseCodeEnum.success.getCode();
+		this.message = MicroApiReserveResponseCodeEnum.success.getMessage();
+	}
 
-    private void error(int code, String message, String stackTrace) {
-        if (code == MicroApiReserveResponseCodeEnum.success.getCode()) {
-            code = MicroApiReserveResponseCodeEnum.unknown_exception.getCode();
-        }
-        this.code = code;
-        this.message = message;
-        this.stackTrace = stackTrace;
-    }
+	private void error(int code, String message, String stackTrace) {
+		if (code == MicroApiReserveResponseCodeEnum.success.getCode()) {
+			code = MicroApiReserveResponseCodeEnum.unknown_exception.getCode();
+		}
+		this.code = code;
+		this.message = message;
+		this.stackTrace = stackTrace;
+	}
 
-    public void error(ResponseCode responseCode) {
-        error(responseCode.getCode(), responseCode.getMessage(), null);
-    }
+	public void error(ResponseCode responseCode) {
+		error(responseCode.getCode(), responseCode.getMessage(), null);
+	}
 
-    public void error(ResponseCode responseCode, Throwable cause) {
-        error(responseCode.getCode(), responseCode.getMessage(), cause == null ? null : ExceptionUtils.getStackTrace(cause));
-    }
+	public void error(ResponseCode responseCode, Throwable cause) {
+		error(responseCode.getCode(), responseCode.getMessage(),
+				cause == null ? null : ExceptionUtils.getStackTrace(cause));
+	}
 
-    public String getApiName() {
-        return apiName;
-    }
+	public String getApiName() {
+		return apiName;
+	}
 
-    public void setApiName(String apiName) {
-        this.apiName = apiName;
-    }
+	public void setApiName(String apiName) {
+		this.apiName = apiName;
+	}
 
-    public String getRequestUuid() {
-        return requestUuid;
-    }
+	public String getRequestUuid() {
+		return requestUuid;
+	}
 
-    public void setRequestUuid(String requestUuid) {
-        this.requestUuid = requestUuid;
-    }
+	public void setRequestUuid(String requestUuid) {
+		this.requestUuid = requestUuid;
+	}
 
-    public int getCode() {
-        return code;
-    }
+	public int getCode() {
+		return code;
+	}
 
-    public void setCode(int code) {
-        this.code = code;
-    }
+	public void setCode(int code) {
+		this.code = code;
+	}
 
-    public String getMessage() {
-        return message;
-    }
+	public String getMessage() {
+		return message;
+	}
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
+	public void setMessage(String message) {
+		this.message = message;
+	}
 
-    public String getStackTrace() {
-        return stackTrace;
-    }
+	public String getStackTrace() {
+		return stackTrace;
+	}
 
-    public void setStackTrace(String stackTrace) {
-        this.stackTrace = stackTrace;
-    }
+	public void setStackTrace(String stackTrace) {
+		this.stackTrace = stackTrace;
+	}
 
-    public long getOverByEngineTimestamp() {
-        return overByEngineTimestamp;
-    }
+	public long getOverByEngineTimestamp() {
+		return overByEngineTimestamp;
+	}
 
-    public void setOverByEngineTimestamp(long overByEngineTimestamp) {
-        this.overByEngineTimestamp = overByEngineTimestamp;
-    }
+	public void setOverByEngineTimestamp(long overByEngineTimestamp) {
+		this.overByEngineTimestamp = overByEngineTimestamp;
+	}
 }
