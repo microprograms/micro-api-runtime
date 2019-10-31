17a8f1ec1e5c7a2d6729271a4f60b0b239f95e48
xuzewei <microprograms@126.com>
2019-10-31 16:03:54 +0800

    v6.0.2

--- src/main/java/com/github/microprograms/micro_api_runtime/model/Api.java
+++ src/main/java/com/github/microprograms/micro_api_runtime/model/Api.java
@@ -1,11 +1,9 @@
 package com.github.microprograms.micro_api_runtime.model;
 
-import com.github.microprograms.micro_api_runtime.exception.PassthroughException;
-
 /**
  * API接口
  */
 public interface Api {
 
-	Response execute(Request request) throws PassthroughException;
+	Response execute(Request request) throws Exception;
 }
